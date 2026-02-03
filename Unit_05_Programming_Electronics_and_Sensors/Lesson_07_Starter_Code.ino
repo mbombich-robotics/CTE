c062cfd89e2ab52ad41e87407d9113cba6d87d17
mbombich@vicksburgcommunityschools.org <mbombich@vicksburgschools.org>
2026-02-03 13:40:32 -0500

    update lesson 7

--- Unit_05_Programming_Electronics_and_Sensors/Lesson_07_Starter_Code.ino
+++ Unit_05_Programming_Electronics_and_Sensors/Lesson_07_Starter_Code.ino
@@ -1,30 +1,57 @@
 // Lesson 7: IR Sensor Fundamentals and Reading
 // Starter Code for Lab Exercises
 
-// IR Sensor pin definitions
-const int SENSOR_1 = 6;   // Far left
-const int SENSOR_2 = 7;   // Left
-const int SENSOR_3 = 8;   // Center
-const int SENSOR_4 = 9;   // Right
-const int SENSOR_5 = 10;  // Far right
+// IR sensor pin definitions
+const int IR_1 = A2;             // IR 1 sensor input
+const int IR_2 = 0;              // IR 2 sensor input
+const int IR_3 = 2;              // IR 3 sensor input
+const int IR_4 = 1;              // IR 4 sensor input
+const int IR_5 = A3;             // IR 5 sensor input
 
 void setup() {
-  // Set all sensor pins as inputs
-  pinMode(SENSOR_1, INPUT);
-  pinMode(SENSOR_2, INPUT);
-  pinMode(SENSOR_3, INPUT);
-  pinMode(SENSOR_4, INPUT);
-  pinMode(SENSOR_5, INPUT);
-
-  // Start serial communication for debugging
   Serial.begin(9600);
   Serial.println("IR Sensor Test Starting...");
+
+  // Set IR sensor pins as inputs
+  pinMode(IR_1, INPUT);
+  pinMode(IR_2, INPUT);
+  pinMode(IR_3, INPUT);
+  pinMode(IR_4, INPUT);
+  pinMode(IR_5, INPUT);
 }
 
 void loop() {
-  // TODO: Read all sensors
-  // TODO: Print sensor values to Serial Monitor
-  // TODO: Add your code here!
+  // Read and print IR sensors as a 5-bit pattern (IR5 IR4 IR3 IR2 IR1)
+  // Open Serial Monitor (9600 baud) to see sensor values
+  if (digitalRead(IR_5) == HIGH) {
+    Serial.print("1");
+  } else {
+    Serial.print("0");
+  }
 
-  delay(200);  // Small delay for readability
+  if (digitalRead(IR_4) == HIGH) {
+    Serial.print("1");
+  } else {
+    Serial.print("0");
+  }
+
+  if (digitalRead(IR_3) == HIGH) {
+    Serial.print("1");
+  } else {
+    Serial.print("0");
+  }
+
+  if (digitalRead(IR_2) == HIGH) {
+    Serial.print("1");
+  } else {
+    Serial.print("0");
+  }
+
+  if (digitalRead(IR_1) == HIGH) {
+    Serial.println("1");
+  } else {
+    Serial.println("0");
+  }
+
+  delay(500);
 }
